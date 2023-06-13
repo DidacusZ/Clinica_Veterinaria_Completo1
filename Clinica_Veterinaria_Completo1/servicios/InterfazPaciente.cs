1c60d03eb52e5132be6f53bf441d7e36f3041d03
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-06-13 14:19:22 +0200

    estructura inicial interfaz cocmentarios

--- Clinica_Veterinaria_Completo1/servicios/InterfazPaciente.cs
+++ Clinica_Veterinaria_Completo1/servicios/InterfazPaciente.cs
@@ -10,8 +10,17 @@ namespace Clinica_Veterinaria_Completo1.servicios
     internal interface InterfazPaciente
     {
 
-
+        //con cabecera en la primera linea
+        //todos los datos separados por comas
+        //si no hay fecha salida se rellena con '----'
         void ListarPacientes(List<Paciente> lista);
+
+
+
+        //Pide todos los datos
+        //guarda los datos en una lista de objetos
+        //lo escribe en un fichero que registra todos los ingresos
+        void IngresoPaciente(List<Paciente> lista);
 
 
     }
